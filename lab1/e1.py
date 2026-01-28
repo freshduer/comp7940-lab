255610b8b776c5ec2d872d809bd65326f9602ac3
Duterfresh <582544712@qq.com>
2026-01-28 11:18:35 +0800

    feat(lab1)

---
+++ lab1/e1.py
@@ -0,0 +1,6 @@
+# Find all the factors of x using a loop and the operator %
+# % means find remainder, for example 10 % 2 = 0; 10 % 3 = 1
+x = 52633
+for i in range(2, x):
+    if x % i == 0:
+        print(i)
